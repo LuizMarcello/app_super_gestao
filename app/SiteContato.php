4942ba626d3d2d07abbc7efa9afd4d323a7b8be8
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-11-15 10:26:52 -0300

    Londrina:15.11.2021_10:26hs

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -1,9 +1,5 @@
 <?php
 
-/* Model é uma classe que vai representar um tipo de dado,
-   um objeto dentro do sistema.
-   Os "Models" são sempre criados no singular e CameYouCase. */
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
