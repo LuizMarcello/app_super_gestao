e6c450fe27d792f430b8b9b7290f8d95e91027e5
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-11-27 10:59:15 -0300

    Londrina:27.11.2021_10:59hs

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -9,5 +9,11 @@ class Fornecedor extends Model
     //Sobrepondo a nomenclatura automática do eloquent a tabela
     //no banco de dados, com base no nome do modelo.
     //O nome da tabela então será 'fornecedores', conforme abaixo.
+    //Atributo "$table":
     protected $table = 'fornecedores';
+
+    //Autoriza o método estático "::create()" a fazer a inserção de registros
+    //no banco de dados, através de um array associativo.
+    //Atributo "#fillable":
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
